c9d75ca53486f6b5128b842b9ab265f331058557
Dana Zangana <danaas@hotmail.com>
2019-11-01 21:51:34 +0100

    Timer is added and start automatically on first input. Added functionality to restart button.

--- src/components/FastFingers.js
+++ src/components/FastFingers.js
@@ -1,17 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import { fromS } from 'hh-mm-ss';
 
+import refresh from '../image/refresh.png'
 import { words } from '../constants/Words';
 import { NEW, CORRECT, WRONG, CURRENT } from '../constants/wordStatus';
 import { Button } from '@material-ui/core';
+import { timeout } from 'q';
 
 const FastFingers = () => {
     const [activeWords, setActiveWords] = useState([]);
     const [currentWord, setCurrentWord] = useState(0);
     const [inputWord, setInputWord] = useState("");
+    const [disableInput, setDisableInput] = useState(false);
     const [wordsCompleted, setWordsCompleted] = useState(0);
     const [correctWords, setCorrectWords] = useState(0);
+    const [timer, setTimer] = useState(60);
+    const [gameOver, setGameOver] = useState(true);
+    let interval;
+
+    const tikTok = () => {
+        setTimer(timer => timer - 1);
+    }
+
+    const restartTimer = () => {
+        setTimer(60);
+    }
+
+    const restart = () => {
+        setGameOver(true);
+        setDisableInput(false);
+        restartTimer();
+        getNewWords();
+        setInputWord("");
+    }
+
+    const startGame = () => {
+        setGameOver(false);
+    }
+
+    const endGame = () => {
+        setGameOver(true);
+        setDisableInput(true);
+    }
+
+    const timeOut = () => {
+        return timer <= 0;
+    }
 
     const getNewWords = () => {
         const newWords = []
@@ -23,6 +59,7 @@ const FastFingers = () => {
             });
         }
         setActiveWords(newWords);
+        setCurrentWord(0);
     };
 
     const validateCurrentWord = () => {
@@ -54,21 +91,43 @@ const FastFingers = () => {
         return activeWords[currentWord].word.includes(inputWord.trim());
     };
 
+    // ComponentDidMount - First time component is renderd
+    useEffect(() => {
+        console.log("ComponentDidMount");
+        
+        restart();
+    }, []);
+
+    // When current word is changed or new words are generated
     useEffect(() => {
         setCurrentWord(currentWord);
     }, [activeWords, currentWord]);
 
+    // Timer starts
+    useEffect(() => {
+        let interval = null;
+        if ( gameOver ) clearInterval(interval);
+        else if ( timeOut() ) endGame();
+        else {
+            interval = setInterval(() => {
+                tikTok();
+            }, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [timer, gameOver]);
+
     return(
         <Grid container alignContent="center" spacing={3}>
-            { activeWords.length === 0 && getNewWords() }
             <Grid item lg={12} className="rowBox"> { activeWords && printRow(activeWords)} </Grid>
             <Grid item lg={12}>
-                <TextField 
+                <TextField
                     autoFocus
+                    disabled={disableInput}
                     className="input"
                     error={!!inputWord && !compareInput()}
                     value={inputWord}
                     onChange={(event) => {
+                        if ( gameOver ) startGame();
                         setInputWord(event.target.value);
                     }}
                     onKeyPress={(event) => {
@@ -79,10 +138,10 @@ const FastFingers = () => {
                     }}
                 />
                 <span className="timer">
-                    timer
+                    { fromS(timer) }
                 </span>
-                <Button>
-                    Start
+                <Button onClick={ restart }>
+                    <img src={refresh} alt="restart" className="refresh-button"/>
                 </Button>
             </Grid>
         </Grid>
